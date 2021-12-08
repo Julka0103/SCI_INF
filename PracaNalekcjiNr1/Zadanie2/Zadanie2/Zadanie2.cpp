b98c737f99e2ed43dea7cc7ee2e1b18dd5a929de
Julka Podolak <julia.podolak04@gmail.com>
2021-12-08 19:00:54 +0100

    Dodanie klas

--- PracaNalekcjiNr1/Zadanie2/Zadanie2/Zadanie2.cpp
+++ PracaNalekcjiNr1/Zadanie2/Zadanie2/Zadanie2.cpp
@@ -2,74 +2,81 @@
 #include <string>
 
 using namespace std;
-
-string cezar(string t) {
-	cout << "Podaj wartosc przesuniecia: \n";
-	int w;
-	for (;;) {
-		cin >> w;
-		if (!(w > 0 && w < 27)) {
-			cout << "Podales zla wartosc\n";
-		}
-		else {
-			break;
-		}
-	}
-
-	for (int i = 0; i < t.length(); i++) {
-		if (t[i] == 32) {
-			continue;
-		}
-		else if (t[i] + w > 122) {
-			t[i] = 96 + ((t[i] + w) - 122);
-		}
-		else {
-			t[i] = t[i] + w;
-		}
-	}
-
-	return t;
-}
-void odszyfrowanie(string t) {
-	string odszyfrowane;
-	for (int i = 1; i < 27; i++) {
-		odszyfrowane = t;
-		for (int k = 0; k < t.length(); k++) {
-			if (t[k] == 32) {
-				continue;
-			}
-			else if (t[k] - i < 97) {
-				odszyfrowane[k] = 122 - (96 - (odszyfrowane[k] - i));
+class Cypher {
+public:
+	string cezar(string t) {
+		cout << "Podaj wartosc przesuniecia: \n";
+		int w;
+		for (;;) {
+			cin >> w;
+			if (!(w > 0 && w < 27)) {
+				cout << "Podales zla wartosc\n";
 			}
 			else {
-				odszyfrowane[k] = odszyfrowane[k] - i;
+				break;
 			}
 		}
-		cout << odszyfrowane << endl;
-	}
-}
 
-string przestawieniowy(string t) {
-	for (int i = 0; i < t.length(); i = i + 2) {
-		if (i + 2 > t.length()) {
-			break;
+		for (int i = 0; i < t.length(); i++) {
+			if (t[i] == 32) {
+				continue;
+			}
+			else if (t[i] + w > 122) {
+				t[i] = 96 + ((t[i] + w) - 122);
+			}
+			else {
+				t[i] = t[i] + w;
+			}
 		}
-		else {
-			swap(t[i], t[i + 1]);
+		return t;
+	}
+
+	string przestawieniowy(string t) {
+		for (int i = 0; i < t.length(); i = i + 2) {
+			if (i + 2 > t.length()) {
+				break;
+			}
+			else {
+				swap(t[i], t[i + 1]);
+			}
+		}
+		return t;
+	}
+
+	string dwa(string t) {
+		string c = cezar(t);
+		c = przestawieniowy(c);
+		return c;
+	}
+};
+
+class Decypher {
+public:
+	void odszyfrowanie(string t) {
+		string odszyfrowane;
+		for (int i = 1; i < 27; i++) {
+			odszyfrowane = t;
+			for (int k = 0; k < t.length(); k++) {
+				if (t[k] == 32) {
+					continue;
+				}
+				else if (t[k] - i < 97) {
+					odszyfrowane[k] = 122 - (96 - (odszyfrowane[k] - i));
+				}
+				else {
+					odszyfrowane[k] = odszyfrowane[k] - i;
+				}
+			}
+			cout << odszyfrowane << endl;
 		}
 	}
-	return t;
-}
-
-string dwa(string t) {
-	string c = cezar(t);
-	c = przestawieniowy(c);
-	return c;
-}
+};
 
 int main()
 {
 	string tekst;
+	Cypher obiekt1;
+	Decypher obiekt2;
 
 	cout << "Podaj tekst na ktorym bedziesz pracowac: [Z malych liter!] \n";
 	getline(cin, tekst);
@@ -84,16 +91,16 @@ int main()
 	cin >> x;
 	switch (x) {
 	case 1:
-		cout << cezar(tekst);
+		cout << obiekt1.cezar(tekst);
 		break;
 	case 2:
-		cout << przestawieniowy(tekst);
+		cout << obiekt1.przestawieniowy(tekst);
 		break;
 	case 3:
-		cout << dwa(tekst);
+		cout << obiekt1.dwa(tekst);
 		break;
 	case 4:
-		odszyfrowanie(tekst);
+		obiekt2.odszyfrowanie(tekst);
 		break;
 	}
 }
